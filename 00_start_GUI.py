dc4164ceba22e28fc4ba7a0ad032d19e6dadbb08
Hazza158 <99371032+Hazza158@users.noreply.github.com>
2024-07-29 14:32:54 +1200

    Add files via upload

--- 00_start_GUI.py
+++ 00_start_GUI.py
@@ -12,41 +12,29 @@ class ChooseRounds:
         self.intro_frame = Frame(padx=10, pady=10)
         self.intro_frame.grid()
 
-        # heading and brief instructions
+        # Heading and brief instructions
         self.intro_heading_label = Label(self.intro_frame, text="Greek Gods Quiz",
                                          font=("Arial", "16", "bold"))
         self.intro_heading_label.grid(row=0)
 
         choose_instructions_txt = "In this quiz you will input the amount of " \
                                   "questions you want to answer, then press the " \
-                                  "'START' button to begin the quiz." \
-
+                                  "'START' button to begin the quiz."
 
         self.choose_instructions_label = Label(self.intro_frame,
                                                text=choose_instructions_txt,
                                                wraplength=300, justify="left")
         self.choose_instructions_label.grid(row=1)
 
-        # Rounds buttons...
-        self.how_many_frame = Frame(self.intro_frame)
-        self.how_many_frame.grid(row=2)
+        self.rounds_entry = Entry(self.intro_frame)
+        self.rounds_entry.grid(row=2, column=0)
 
-        # list to set up rounds button.  First item in each
-        # sublist is the background color, second item is
-        # the number of rounds
-        btn_color_value = [
-            ["#009900", 1]
-            ]
-
-        for item in range(0, 1):
-            self.rounds_button = Button(self.how_many_frame,
-                                        fg=button_fg, bg=btn_color_value[item][0],
-                                        text="START".format(btn_color_value[item][1]),
-                                        font=button_font, width=10,
-                                        command=lambda i=item: self.to_play(btn_color_value[i][1])
-                                        )
-            self.rounds_button.grid(row=0, column=item,
-                                    padx=5, pady=5)
+        # Start button to initiate the quiz
+        self.start_button = Button(self.intro_frame, text="START",
+                                   fg=button_fg, bg="#009900",
+                                   font=button_font,
+                                   command=self.start_quiz)
+        self.start_button.grid(row=3, columnspan=2, pady=10)
 
     def start_quiz(self):
         try:
@@ -65,9 +53,8 @@ class ChooseRounds:
     def to_play(self, num_rounds):
         Play(num_rounds)
 
-        # Hide root window (ie: hide rounds choice window).
+        # Hide root window (i.e., hide rounds choice window).
         root.withdraw()
-
 
 
 class Play:
@@ -75,8 +62,8 @@ class Play:
     def __init__(self, how_many):
         self.play_box = Toplevel()
 
-        # If users press cross at top, closes help and
-        # 'releases' help button
+        # If users press cross at top, close help and
+        # 'release' help button
         self.play_box.protocol('WM_DELETE_WINDOW',
                                partial(self.close_play))
 
@@ -85,8 +72,7 @@ class Play:
 
         rounds_heading = "Choose - Round 1 of {}".format(how_many)
         self.choose_heading = Label(self.quest_frame, text=rounds_heading,
-                                    font=("Arial", "16", "bold")
-                                    )
+                                    font=("Arial", "16", "bold"))
         self.choose_heading.grid(row=0)
 
         self.control_frame = Frame(self.quest_frame)
@@ -97,13 +83,13 @@ class Play:
         self.start_over_button.grid(row=0, column=2)
 
     def close_play(self):
-        # reshow root (ie: choose rounds) and end current
+        # Reshow root (i.e., choose rounds) and end current
         # game / allow new game to start
         root.deiconify()
         self.play_box.destroy()
 
 
-# main routine
+# Main routine
 if __name__ == "__main__":
     root = Tk()
     root.title("Greek Gods Quiz")
